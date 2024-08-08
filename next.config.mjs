2b1830cd8c3bd7eab1a83383f099b7d0f2c7402a
saim-x <x_aci@hotmail.com>
2024-08-08 15:01:23 +0500

    hi

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['saimfolio.vercel.app', 'images.pexels.com'], // Add your image domain here
+    },
+};
 
 export default nextConfig;
